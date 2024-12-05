1fd52396c8c8a5aede897bc59cdb4d83eae1301d
akangromeo <deanagnia7@gmail.com>
2024-12-05 19:11:22 +0800

    update port

--- config/database.js
+++ config/database.js
@@ -5,11 +5,11 @@ const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASSWORD,
-  process.env.DB_PORT,
 
   {
     host: process.env.DB_HOST,
     dialect: "mysql",
+    port: process.env.DB_PORT,
     logging: false,
   }
 );
